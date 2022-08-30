76ce567fb3fdba1c3d2ceeeb3d6f5fe39399c7c9
developiaa <developiaa@gmail.com>
2022-08-30 23:08:48 +0900

    feat: Add test config, item repository test, init data

--- src/main/java/study/developia/db/DbApplication.java
+++ src/main/java/study/developia/db/DbApplication.java
@@ -2,12 +2,24 @@ package study.developia.db;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
+import study.developia.db.config.MemoryConfig;
+import study.developia.db.repository.ItemRepository;
 
-@SpringBootApplication
+@Import(MemoryConfig.class)
+@SpringBootApplication(scanBasePackages = "study.developia.db.web")
 public class DbApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DbApplication.class, args);
+    }
+
+    @Bean
+    @Profile("local")
+    public TestDataInit testDataInit(ItemRepository itemRepository) {
+        return new TestDataInit(itemRepository);
     }
 
 }
---
+++ src/main/java/study/developia/db/TestDataInit.java
@@ -0,0 +1,25 @@
+package study.developia.db;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import study.developia.db.domain.Item;
+import study.developia.db.repository.ItemRepository;
+
+@Slf4j
+@RequiredArgsConstructor
+public class TestDataInit {
+
+    private final ItemRepository itemRepository;
+
+    /**
+     * 확인용 초기 데이터 추가
+     */
+    @EventListener(ApplicationReadyEvent.class)
+    public void initData() {
+        log.info("test data init");
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+    }
+}
