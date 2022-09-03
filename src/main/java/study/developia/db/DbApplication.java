6ec3853bbeea6c26f006c0d1f9bb7f05bba59f9b
developiaa <developiaa@gmail.com>
2022-09-03 20:03:57 +0900

    feat: Apply NamedParameterJdbcTemplate #2

--- src/main/java/study/developia/db/DbApplication.java
+++ src/main/java/study/developia/db/DbApplication.java
@@ -6,11 +6,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 import study.developia.db.config.JdbcTemplateV1Config;
+import study.developia.db.config.JdbcTemplateV2Config;
 import study.developia.db.config.MemoryConfig;
 import study.developia.db.repository.ItemRepository;
 
 //@Import(MemoryConfig.class)
-@Import(JdbcTemplateV1Config.class)
+//@Import(JdbcTemplateV1Config.class)
+@Import(JdbcTemplateV2Config.class)
 @SpringBootApplication(scanBasePackages = "study.developia.db.web")
 public class DbApplication {
 
