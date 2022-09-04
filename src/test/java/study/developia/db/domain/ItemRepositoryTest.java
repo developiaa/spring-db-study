fdc638bb99c98404a21ae79f68158f19eec73781
developiaa <developiaa@gmail.com>
2022-09-04 16:32:58 +0900

    feat: Modify test config, transaction

--- src/test/java/study/developia/db/domain/ItemRepositoryTest.java
+++ src/test/java/study/developia/db/domain/ItemRepositoryTest.java
@@ -1,9 +1,13 @@
 package study.developia.db.domain;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 import study.developia.db.repository.ItemRepository;
 import study.developia.db.repository.ItemSearchCond;
 import study.developia.db.repository.ItemUpdateDto;
@@ -19,12 +23,26 @@ class ItemRepositoryTest {
     @Autowired
     ItemRepository itemRepository;
 
+    // 트랜잭션 관련 코드
+    @Autowired
+    PlatformTransactionManager transactionManager;
+    TransactionStatus status;
+
+    @BeforeEach
+    void beforeEach() {
+        // 트랜잭션 시작
+        status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+    }
+
     @AfterEach
     void afterEach() {
         //MemoryItemRepository 의 경우 제한적으로 사용
         if (itemRepository instanceof MemoryItemRepository) {
             ((MemoryItemRepository) itemRepository).clearStore();
         }
+
+        // 트랜잭션 롤백
+        transactionManager.rollback(status);
     }
 
     @Test
