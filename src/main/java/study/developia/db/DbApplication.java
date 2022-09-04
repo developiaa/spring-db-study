91dae71c3e4130f586dcac9a292e3fb46a40af74
developiaa <developiaa@gmail.com>
2022-09-04 18:22:18 +0900

    feat: Modify embedded db config #2

--- src/main/java/study/developia/db/DbApplication.java
+++ src/main/java/study/developia/db/DbApplication.java
@@ -33,17 +33,17 @@ public class DbApplication {
         return new TestDataInit(itemRepository);
     }
 
-    // 임베디드 모드 DB
-    @Bean
-    @Profile("test")
-    public DataSource dataSource() {
-        log.info("메모리 데이터베이스 초기화");
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.h2.Driver");
-        dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
-        dataSource.setUsername("sa");
-        dataSource.setPassword("");
-        return dataSource;
-    }
+//    // 임베디드 모드 DB
+//    @Bean
+//    @Profile("test")
+//    public DataSource dataSource() {
+//        log.info("메모리 데이터베이스 초기화");
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName("org.h2.Driver");
+//        dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+//        dataSource.setUsername("sa");
+//        dataSource.setPassword("");
+//        return dataSource;
+//    }
 
 }
