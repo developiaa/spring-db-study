90f52e60501757e0b49987703ac293a00c36280b
developiaa <developiaa@gmail.com>
2022-09-04 17:30:45 +0900

    feat: Add embedded db config, test schema

--- src/main/java/study/developia/db/DbApplication.java
+++ src/main/java/study/developia/db/DbApplication.java
@@ -1,16 +1,21 @@
 package study.developia.db;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import study.developia.db.config.JdbcTemplateV1Config;
 import study.developia.db.config.JdbcTemplateV2Config;
 import study.developia.db.config.JdbcTemplateV3Config;
 import study.developia.db.config.MemoryConfig;
 import study.developia.db.repository.ItemRepository;
 
+import javax.sql.DataSource;
+
+@Slf4j
 //@Import(MemoryConfig.class)
 //@Import(JdbcTemplateV1Config.class)
 //@Import(JdbcTemplateV2Config.class)
@@ -26,6 +31,19 @@ public class DbApplication {
     @Profile("local")
     public TestDataInit testDataInit(ItemRepository itemRepository) {
         return new TestDataInit(itemRepository);
+    }
+
+    // 임베디드 모드 DB
+    @Bean
+    @Profile("test")
+    public DataSource dataSource() {
+        log.info("메모리 데이터베이스 초기화");
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("");
+        return dataSource;
     }
 
 }
